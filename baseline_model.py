83660f878a6d92036bc261d25ac38bc41412c9ba
FionaWennberg <s246024@dtu.dk>
2026-02-28 18:27:59 +0100

    tests and modifications to model iterations

--- baseline_model.py
+++ baseline_model.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import LeaveOneGroupOut, cross_val_predict
 from sklearn.metrics import roc_auc_score
 from data import load_data
 
-def random_search_rf_baseline(X, y, groups, n_iters=19, random_state=42):
+def random_search_rf_baseline(X, y, groups, n_iters=20, random_state=42):
     rng = np.random.default_rng(random_state)
     cv = LeaveOneGroupOut()
 
@@ -69,10 +69,12 @@ def random_search_rf_baseline(X, y, groups, n_iters=19, random_state=42):
 
 X, y, groups = load_data("egg_data_assignment_2", sample_rate=50, sample_rate_original=250)
 
-# Random baseline (19 iterations)
-baseline = random_search_rf_baseline(X, y, groups, n_iters=19, random_state=42)
+# Random baseline (20 iterations)
+baseline = random_search_rf_baseline(X, y, groups, n_iters=20, random_state=42)
 print("\nRANDOM BASELINE SUMMARY")
 print("Mean AUC:", baseline["mean_auc"])
 print("Std AUC:", baseline["std_auc"])
 print("Best AUC:", baseline["best_auc"])
 print("Best params:", baseline["best_params"])
+
+print(baseline["all_auc"])
